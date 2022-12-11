8802088d2e7d4f2054bed3022a4c4333038ec5d6
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2022-12-10 17:38:36 -0800

    done

--- app.js
+++ app.js
@@ -2,16 +2,22 @@
 
 // set event listeners
 
-
-const button = document.getElementById('update-button');
+const nameDisplay = document.getElementById('name-display');
+const button = document.getElementById('updateButton');
 const nameInput = document.getElementById('name-input');
-//const button = document.getElementById('pronoun-button');
-//const pronounInput = document.getElementById('pronoun-input');
+const pronounInput = document.getElementById('pronoun-input');
+const pronounDisplay = document.getElementById('Pronoun');
 
 button.addEventListener('click', () => {
-const nameDisplay = document.getElementById('name-display');
-console.log(nameDisplay);
-nameDisplay.textContent = nameInput.value;
-nameDisplay.style.color = 'black';
+    const nameDisplay = document.getElementById('name-display');
+    console.log(nameDisplay);
+    nameDisplay.textContent = nameInput.value;
+    nameDisplay.style.color = 'black';
 });
 
+button.addEventListener('click', () => {
+    const pronounDisplay = document.getElementById('pronoun-display');
+    console.log(pronounDisplay);
+    pronounDisplay.textContent = pronounInput.value;
+    pronounDisplay.style.color = 'black';
+});

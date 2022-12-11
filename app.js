fecc05690b33b2c26d7e5ea477257d8c628551c7
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2022-12-10 16:36:42 -0800

    cant figure it out

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 // grab DOM elements
 
 // set event listeners
+
+
+const button = document.getElementById('update-button');
+const nameInput = document.getElementById('name-input');
+const otherbutton = document.getElementById('pronoun-button');
+const pronounInput = document.getElementById('pronoun-input');
+button.addEventListener('click', () => {
+const nameDisplay = document.getElementById('name-display');
+console.log(nameDisplay);
+nameDisplay.textContent = nameInput.value;
+nameDisplay.style.color = 'black';
+});
+button.addEventListener('click', () => {
+const pronounDisplay.textContent = pronounInput.value;
+console.log(pronounDisplay);
+nameDisplay.textContent = pronounInput.value;
+nameDisplay.style.color = 'black';
+});
---
+++ datatypes.js
@@ -0,0 +1,17 @@
+// strings
+let answer = '42';
+let dogName = 'Benny';
+console.log(`My dogs name is ${dogName}. ${dogName} is very cute.`); 
+
+// arrays
+let colors = ['red', 'green', 'blue'];
+console.log(colors);
+console.log(colors[0]);
+
+// objects
+let benny = {
+    name: 'Benny',
+    age: 7,
+    breeds: ['Terrier', 'Chihauhau', 'Pekingese'],
+};
+console.log(benny);
